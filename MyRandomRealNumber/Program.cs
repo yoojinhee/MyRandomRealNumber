5035e62733fc76750d87f30396f786cc4bb74f0c
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-19 15:50:28 +0900

    Random함수 실습

--- MyRandomRealNumber/Program.cs
+++ MyRandomRealNumber/Program.cs
@@ -10,6 +10,31 @@ namespace MyRandomRealNumber
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int correct = random.Next(1, 500);
+            Boolean chk = true;
+            int answer;
+            while (chk)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                answer = Int32.Parse(Console.ReadLine());
+
+                if (correct == answer)
+                {
+                    Console.WriteLine("정답입니다!");
+                    chk = false;
+                }else if (correct < answer)
+                {
+                    Console.WriteLine(answer+"보다는 작은수입니다!");
+
+                }
+                else
+                {
+                    Console.WriteLine(answer + "보다는 큰수입니다!");
+                }
+
+            }
+
         }
     }
 }
